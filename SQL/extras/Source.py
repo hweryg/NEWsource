d1d9fe4980c6e8f9836044eac4be05c3ebfeaba2
hweryg <99652118+hweryg@users.noreply.github.com>
2022-02-18 16:07:27 +0200

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,12 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**⎈ ⦙ كيفيه التنصيب :**")
+help2 = ("**⎈ ⦙ قـائمـه الاوامـر :**\n**⎈ ⦙ قنـاه السـورس :** @SU_SELVA\n**⎈ ⦙ شـرح اوامـر السـورس : @L3LL3**\n**⎈ ⦙ شـرح فـارات السـورس : @TEAMTELETHON** ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**.𓄌 : version 7.4  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : 𓄼•𝐒𝐎𝐔𝐑𝐂𝐄 𝐒𝐄𝐋𝐕𝐀•𓄹 : @SU_SELVA  𓇡.**"
+ 
